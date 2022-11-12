359ef6be091eaeecc047d004b5758e31180d6d24
hamzat <lawalhamzat27@gmail.com>
2022-11-12 18:05:57 +0100

    updated stage3 task

--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -4,13 +4,13 @@ export const FooterComponent=()=>{
             <div className="d-flex flex-column footer-bg">
                 <div>
                     <div className="row mobile_align">
-                        <div className="col-md-4 col-lg-3 col-xs-6">
+                        <div className="col-md-4 col-lg-3 col-sm-6">
                             <div className="d-flex mb-5">
                                 <span className="mb-0"><img className="badge_img"  src="https://res.cloudinary.com/hamskid/image/upload/v1668255776/Vector_6_f2ap9k.png" alt="object not found"/></span>
                                 <span><h1 className="text-white fw-bold" >Metabnb</h1></span>
                             </div>
                             <div>
-                            <div className=" flex-column web-footer justify-content-center align-items-center">
+                            <div className="flex-column web-footer justify-content-center align-items-center">
                                 <div className="mb-5">
                                     <div className="d-flex">
                                         <span className="me-4">
@@ -37,7 +37,7 @@ export const FooterComponent=()=>{
                             </div>
                         </div>   
                         </div> 
-                        <div className="col-md-4 col-lg-3 col-xs-6">
+                        <div className="col-md-4 col-lg-3 col-sm-6">
                             <div className="footer_li">
                                 <h6 className="fw-bold link_head ">Community</h6>
                                 <h6><a href="#">NFTs</a></h6>
@@ -46,7 +46,7 @@ export const FooterComponent=()=>{
                                 <h6><a href="#">Discord</a></h6>
                             </div>    
                         </div>
-                        <div className="col-md-4 col-lg-3 col-xs-6">
+                        <div className="col-md-4 col-lg-3 col-sm-6">
                             <div className="footer_li">
                                 <h6 className="fw-bold link_head">Places</h6>
                                 <h6><a href="#">Castle</a></h6>
@@ -55,7 +55,7 @@ export const FooterComponent=()=>{
                                 <h6><a href="#">Learn more</a></h6>
                             </div>   
                         </div>
-                        <div className="col-md-4 col-lg-3 col-xs-6">
+                        <div className="col-md-4 col-lg-3 col-sm-6">
                             <div className="footer_li">
                                 <h6 className="fw-bold link_head ">Abouts</h6>
                                 <h6><a href="#">RoadMaps</a></h6>
@@ -81,7 +81,6 @@ export const FooterComponent=()=>{
                                         <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-twitter" viewBox="0 0 16 16">
                                             <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
                                         </svg> </a>
-                                          
                                     </span>
                                 </div>
                             <div>
